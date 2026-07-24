088ecd24d76807ce95deea388656c3030eff8a0b
Efojensen <jensena231@gmail.com>
2026-07-24 19:38:25 +0000

    modified signUp route to create wallet record after also creating the user record

--- routes/clients/store.go
+++ routes/clients/store.go
@@ -12,18 +12,44 @@ func (c *ClientHandler) addClientQuery(client types.Client) error {
 	ctx, timeout := context.WithTimeout(context.Background(), 5 * time.Second)
 	defer timeout()
 
+	tx, err := c.store.Begin(ctx)
+
+	if err != nil {
+		log.Println("error creating pg transaction")
+		return err
+	}
+
+	defer func(){
+		tx.Rollback(ctx)
+	}()
+
 	insertQuery := `
 		INSERT INTO users (name, email, phone_number, password, profile_pic)
 		VALUES ($1, $2, $3, $4, $5)
+		RETURNING user_id
 	`
 
-	commandTag, err := c.store.Exec(ctx, insertQuery, client.Name, client.Email, client.PhoneNumber,
-		client.Password, client.ProfilePic)
+	var user_id string
+	err = tx.QueryRow(ctx, insertQuery, client.Name, client.Email, client.PhoneNumber,
+		client.Password, client.ProfilePic).Scan(&user_id)
 
 	if err != nil {
 		return err
 	}
 
-	log.Println(commandTag.RowsAffected(), "rows affected in \"users\" table")
+	insertWalletQuery := `INSERT INTO wallets (user_id) VALUES ($1)`
+
+	conn, err := tx.Exec(ctx, insertWalletQuery, user_id)
+
+	if err != nil {
+		log.Println(conn.String())
+		return err
+	}
+
+	if err = tx.Commit(ctx); err != nil {
+		log.Println("error while committing transaction")
+		return err
+	}
+
 	return nil
 }
